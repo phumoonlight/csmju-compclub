466ce34eadd6b34459739fcf2fbeac563f179f26
Phusan Moonmuang <phumoonlight@hotmail.com>
2019-06-28 23:40:10 +0700

    Create layout.php

---
+++ src/components/layout.php
@@ -0,0 +1,3 @@
+<section class="navbar"><?php require_once './navbar.html' ?></section>
+<section class="title">ชมรมคอมพิวเตอร์</section>
+<section class="menu"><?php require_once './menu.html' ?></section>
